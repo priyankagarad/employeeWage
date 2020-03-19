7a0a6f69f0225288e59cc08fdebc3cb42040434d
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-19 23:24:32 +0530

    Added : calculated monthly wage for employee

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,35 +1,31 @@
 #!/bin/bash -x
 
 wage_per_hour=20
+days=0;
+hours=0;
+totalSalary=0;
 
 echo "Welcome to employee Wage Computation "
 
 #attendance check
-attendance=$((RANDOM%3))
-echo $attendance
-if [[ $attendance -eq 1 ]]
-then
-	full_day_hour=8;
-elif [[ $attendance -eq 2 ]]
-then
-	echo "Absent"
-else
-	part_time_hour=4;
-fi
 
-read -p "enter choice:" choice
-case $choice in
-		0)
-			daily_employee_wage=$(($wage_per_hour*$full_day_hour))
-			part_time_employee_wage=$(($wage_per_hour*$part_time_hour))
-			;;
-
-		1)
-			daily_employee_wage=$(($wage_per_hour*$full_day_hour))
-			part_time_employee_wage=$(($wage_per_hour*$full_day_hour))
-			;;
-
-		2)
-			echo "employee is absent"
-			;;
-esac
+while [[ $days -le 20 && $hours -le 100 ]]
+	do
+	attendance=$((RANDOM%3))
+	if [[ $attendance -eq 1 ]]
+	then
+		hour=8;
+		hours=$((hours+full_day_hour))
+		((days++))
+	elif [[ $attendance -eq 2 ]]
+	then
+		echo "Absent"
+		hours=$((hours+0))
+	else
+		hour=4;
+		((days++))
+		hours=$((hours+4))
+	fi
+	daily_employee_wage=$(($wage_per_hour*$hour))
+	totalSalary=$((totalSalary+daily_employee_wage))
+done
