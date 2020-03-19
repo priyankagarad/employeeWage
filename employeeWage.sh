13bfa235156ab37eaa9f88a6498bcf7200f37bed
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-19 23:04:16 +0530

    Added : calculate part time for an employee

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,20 +1,26 @@
-#!/bin/bash
+#!/bin/bash -x
+
 wage_per_hour=20
-full_day_hour=8
 
 echo "Welcome to employee Wage Computation "
 
-#check employee is present or absent
-attendance=$((RANDOM%2))
+#attendance check
+attendance=$((RANDOM%3))
 echo $attendance
 if [[ $attendance -eq 1 ]]
 then
-	echo "employee is present"
+	full_day_hour=8;
+elif [[ $attendance -eq 2 ]]
+then
+	echo "Absent"
 else
-	echo "employee is absent"
+	part_time_hour=4;
 fi
 
 #calculate daily employee wage
-daily_employee_wage=$(($wage_per_hour*$full_day_hour))
-echo "daily employee wage:$daily_employee_wage"
+result=$(($wage_per_hour*$full_day_hour))
+echo $result
 
+#part time employee wage
+part_time_employee_wage=$((wage_per_hour*part_time_hour))
+echo "part time employee wage:$part_time_employee_wage"
