ee7648a9d70813461a4316f63f29394d1516080c
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-20 13:55:11 +0530

    Added:calculated day and daily wage and stored in dictionary

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,45 +1,48 @@
 #!/bin/bash -x 
 
 #CONSTANT
-wage_per_hour=20
+WAGE_PER_HOUR=20
+PRESENT=1
+PART_TIME=2
+ABSENT=0
 
 #VARIABLE
 days=0;
 hours=0;
 employee_hour=0
-present=1
-partTime=2
-absent=0
 
 echo "Welcome to employee Wage Computation "
 
 function getWorkHour()
 {
 	case $1 in
-			$present)
+			$PRESENT)
 					employee_hour=8
 					echo $employee_hour;
 					;;
 
-			$partTime)
+			$PART_TIME)
 					employee_hour=4
 					echo $employee_hour;
 					;;
 
-			0)
+			$ABSENT)
 					employee_hour=0
 					echo $employee_hour;
 					;;
 	esac
 }
-while [[ $days -le 20 && $hour -le 100 ]]
+
+while [[ $days -le 20 && $hours -le 100 ]]
 do
 	attendance=$((RANDOM%3))
 	employee_hour=$( getWorkHour $attendance ) 
 	hours=$((hours+$employee_hour))
-	daily_wage_hour=$(( hours*wage_per_hour ))
-	daily_wage_hour[$days]=$dail_wage_hour;
+	daily_wage=$(( hours*WAGE_PER_HOUR ))
+	daily_wage[$days]=$daily_wage;
 	((days++))
-	monthly_wage=$(( daily_wage_hour+monthly_wage ))
+	monthly_wage=$(( daily_wage+monthly_wage ))
 done
-echo "${daily_wage_hour[@]}"
+echo -e "${!daily_wage[@]}: \n ${daily_wage[@]}"
+
+
