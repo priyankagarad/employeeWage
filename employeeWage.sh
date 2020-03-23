5224d734fb61e4fdd33092731a5bb20d44c39619
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-23 14:28:15 +0530

    Refactor:adding comment message

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,45 +1,48 @@
 #!/bin/bash -x 
 
 #CONSTANT
-wage_per_hour=20
+WAGE_PER_HOUR=20
+PRSENT=1
+PART_TIME=2
+ABSENT=0
 
 #VARIABLE
 days=0;
 hours=0;
 employee_hour=0
-present=1
-partTime=2
-absent=0
+monthly_wage=0
+daily_wage=0
 
 echo "Welcome to employee Wage Computation "
 
+#using function calculate calculate total wage
 function getWorkHour()
 {
 	case $1 in
-			$present)
-					employee_hour=8
-					echo $employee_hour;
+			$PRESENT)
+					EMPLOYEE_HOUR=8
+					echo $EMPLOYEE_HOUR;
 					;;
 
-			$partTime)
-					employee_hour=4
-					echo $employee_hour;
+			$PART_TIME)
+					EMPLOYEE_HOUR=4
+					echo $EMPLOYEE_HOUR;
 					;;
 
-			0)
-					employee_hour=0
-					echo $employee_hour;
+			$ABSENT)
+					EMPLOYEE_HOUR=0
+					echo $EMPLOYEE_HOUR;
 					;;
 	esac
 }
-while [[ $days -le 20 && $hour -le 100 ]]
+while [[ $days -le 20 && $hours -le 100 ]]
 do
 	attendance=$((RANDOM%3))
-	employee_hour=$( getWorkHour $attendance ) 
+	employee_hour=$( getWorkHour $attendance )
 	hours=$((hours+$employee_hour))
-	daily_wage_hour=$(( hours*wage_per_hour ))
-	daily_wage_hour[$days]=$dail_wage_hour;
+	daily_wage=$(( hours*WAGE_PER_HOUR ))
+	daily_wage[$days]=$daily_wage;
 	((days++))
-	monthly_wage=$(( daily_wage_hour+monthly_wage ))
+	monthly_wage=$(( daily_wage+monthly_wage ))
 done
 echo "${daily_wage_hour[@]}"
